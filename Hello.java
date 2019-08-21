97752575b4077b4be8c173bd894f1a4689efff38
christineangeles <54365080+christineangeles@users.noreply.github.com>
2019-08-21 10:34:09 -0400

    Add files via upload

---
+++ Hello.java
@@ -0,0 +1,26 @@
+/*******************************************************
+ * Hello.world
+********************************************************
+ * Hello
+ * Christine Angeles
+ * 8/21/19
+ * CMSC 255 Section 3
+********************************************************/
+
+public class Hello {
+ public static void printHeading(){
+  System.out.println("Christine Angeles");
+  System.out.println("Hello.world");
+  System.out.println("Hello");
+  System.out.println("8/21/19");
+  System.out.println("CMSC 255 Section 3");
+  System.out.println();
+  }
+  
+ public static void main(String [] args) {
+  printHeading();
+  System.out.println("Hello");
+  }
+}
+
+
